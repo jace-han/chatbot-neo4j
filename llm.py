13f4fda94bd6b26e235b46fcde139c464dae84a6
jace-han <jacehanyy@gmail.com>
2025-06-29 11:34:09 +0800

    Use OpenRouter Properly

--- llm.py
+++ llm.py
@@ -7,6 +7,7 @@ from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(
     openai_api_key=st.secrets["OPENROUTER_API_KEY"],
     model=st.secrets["OPENAI_MODEL"],
+    openai_api_base="https://openrouter.ai/api/v1"
 )
 
 # Create the Embedding model
